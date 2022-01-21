b2fed99aaadcb7f9ce2a55aa218afe1c0cd12ee5
Sehar Asghar <seharasghar39@gmail.com>
2022-01-21 11:43:39 +0500

    idea pool
    idea pool

--- src/components/IdeaPool/IdeaPool.jsx
+++ src/components/IdeaPool/IdeaPool.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import { Grid } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core'
+import IdeaPage from './IdeaPage'
+const useStyle = makeStyles({
+    container: {
+        margin: '15px'
+    }
+})
 export default function IdeaPool() {
+    const classes = useStyle()
     return (
-        <div>
-            <h1>This is the Idea POOl Page</h1>
-        </div>
+        <Grid container>
+        <Grid item xs={0} sm={1}></Grid>
+        <Grid item xs={12} sm={10} className={classes.container} >
+            <IdeaPage />
+        </Grid>
+        <Grid item xs={0} sm={1}></Grid>
+    </Grid>  
     )
 }
